a5fc842002d261fa823152f92a185ee211af69c3
Maxim <54141415+max-madrat@users.noreply.github.com>
2021-12-14 23:40:09 +0300

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -39,7 +39,7 @@ def test_min_number():
     print()
     for s in list_of_int:
         start_time = datetime.now()
-        assert min_number(list_of_int) == 6
+        assert min_number(list_of_int) == min(list_of_int)
         print(sys.getsizeof(s), 'bytes  ', datetime.now() - start_time)
 
 
@@ -63,7 +63,7 @@ def test_total_sum():
     print()
     for s in list_of_int:
         start_time = datetime.now()
-        assert total_sum(s) == sum(s)
+        assert total_sum(s) == 0
         print(sys.getsizeof(s), 'bytes  ', datetime.now() - start_time)
 
 
